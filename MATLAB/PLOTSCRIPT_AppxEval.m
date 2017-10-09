234064e77209b59737af62dd918314b18fec0cd9
Kory Teague <31902344+KoryTeague@users.noreply.github.com>
2017-10-09 15:27:22 -0400

    PLOTSCRIPT_AppxEval Output Fix
    Adds index to the terminal output to aid in legibility.

--- MATLAB/PLOTSCRIPT_AppxEval.m
+++ MATLAB/PLOTSCRIPT_AppxEval.m
@@ -14,5 +14,5 @@ for index = 1:betarng
 sat(index) = satis(del_VorAppxEval{index}, demand);
 tmp = sum(del_VorAppxEval{index}(:, :, 1), 1)';
 Plot_VorMod_Grad(figure, BS, tmp, BS_cap, field)
-fprintf('Max: %1.5f\t%1.5f\nMin: %1.5f\t%1.5f\n', max(tmp), max(tmp) / BS_cap, min(tmp(tmp > 0)), min(tmp(tmp > 0)) / BS_cap)
+fprintf('%d\nMax: %1.5f\t%1.5f\nMin: %1.5f\t%1.5f\n', index, max(tmp), max(tmp) / BS_cap, min(tmp(tmp > 0)), min(tmp(tmp > 0)) / BS_cap)
 end
