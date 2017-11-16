33c6dae4798f292cea1a64f963b53767584a3646
Kory Teague <31902344+KoryTeague@users.noreply.github.com>
2017-11-15 23:51:03 -0500

    Added Generate Members Method

--- MATLAB/GeneticAlgorithm.m
+++ MATLAB/GeneticAlgorithm.m
@@ -9,9 +9,7 @@ classdef GeneticAlgorithm < handle
     %   Member Uniquenes; and able to cycle until a specified halting
     %   condition.
     %   Constructors:
-    %       obj = GeneticAlgorithm() is the default constructor.  This
-    %           generates an empty or default genetic algorithm
-    %       obj = GeneticAlgorithm(...)
+    %       obj = GeneticAlgorithm(nMembers, memberLength, ...)
     
     properties (SetAccess=immutable, GetAccess=public)
         nMembers
@@ -34,14 +32,26 @@ classdef GeneticAlgorithm < handle
                 % first element of pair is string label of flag to be set
                 % second element is the value the flag is to be set to
             % Valid flags to set:
-                % 'nElitism', 'Elitism' -   integer nonnegative
-                % 'flagUniqueness', 'Uniqueness'
-                %                       -   binary; 0 is off, >0 is on
-            obj.nMembers = nMembers;
-            obj.memberLength = memberLength;
+                % 'nElitism', 'Elitism', 'Elite', 'elitism', 'elite'    -
+                %                       -   integer nonnegative
+                % 'flagUniqueness', 'Uniqueness', 'Unique', 'uniqueness',
+                %   'unique'            -   binary; 0 is off, >0 is on (1)
+            % Parse Inputs
+            if isnumeric(nMembers)
+                obj.nMembers = nMembers;
+            else
+                error('geneticalgorithm:constructor:invVal',    ...
+                    'Invalid Value.\nnMembers is a nonnegative integer.\n')
+            end
+            if isnumeric(memberLength)
+                obj.memberLength = memberLength;
+            else
+                error('geneticalgorithm:constructor:invVal',    ...
+                    'Invalid Value.\nmemberLength is a nonnegative integer.\n')
+            end
             for iArg = 1:2:nargin-2
                 switch varargin{iArg}
-                    case {'nElitism', 'Elitism'}
+                    case {'nElitism', 'Elitism', 'Elite', 'elitism', 'elite'}
                         if isnumeric(varargin{iArg+1})
                             if varargin{iArg+1} >= 0
                                 obj.nElitism = int8(varargin{iArg+1});
@@ -53,7 +63,8 @@ classdef GeneticAlgorithm < handle
                             error('geneticalgorithm:constructor:invVal',    ...
                                 'Invalid Value.\nElitism is a nonnegative integer.\n')
                         end
-                    case {'flagUniqueness', 'Uniqueness'}
+                    case {'flagUniqueness', 'Uniqueness', 'Unique', ...
+                            'uniqueness', 'unique'}
                         if isnumeric(varargin{iArg+1})
                             if varargin{iArg+1} >= 0
                                 obj.flagUniqueness = varargin{iArg+1};
@@ -70,8 +81,26 @@ classdef GeneticAlgorithm < handle
                             'Invalid Flag.\nSee documentation for valid flags.\n')
                 end
             end
+            obj.generateMembers();
+        end
+    end
+    methods (Access=private)
+        function generateMembers(obj)
+            if obj.flagUniqueness
+                obj.members = zeros(obj.nMembers, obj.memberLength);
+                iMem = 1;
+                while iMem <= obj.nMembers
+                    tmpMember = randi(2, 1, obj.memberLength) - 1;
+                    if ~ismember(tmpMember, obj.members(1:iMem-1, :),   ...
+                            'rows')
+                        obj.members(iMem, :) = tmpMember;
+                        iMem = iMem + 1;
+                    end
+                end
+            else
+                obj.members = randi(2, obj.nMembers, obj.memberLength) - 1;
+            end
         end
     end
     
 end
-
